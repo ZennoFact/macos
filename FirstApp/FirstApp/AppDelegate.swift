a801b0e409d1e8a60f9ae0d5c8a512118ec89e5d
ZennoFact <zenno04.g@gmail.com>
2015-08-08 02:15:52 +0900

    First commit

---
+++ FirstApp/FirstApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  FirstApp
+//
+//  Created by 前納　一希 on 2015/08/07.
+//  Copyright (c) 2015年 前納 一希. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ FirstApp/FirstApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FirstApp
+//
+//  Created by 前納　一希 on 2015/08/07.
+//  Copyright (c) 2015年 前納 一希. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
